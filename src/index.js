4c94d68f67a15f86f80253a83288ac0c49c6cb70
frontending <mymage259@gmail.com>
2021-03-09 22:29:05 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,12 +1,25 @@
 
 exports.min = function min (array) {
-  return 0;
+  if (array === undefined || array[0] === undefined) {
+    return 0
+  }
+  return Math.min(...array)
 }
 
 exports.max = function max (array) {
-  return 0;
+  if (array === undefined || array[0] === undefined) {
+    return 0
+  }
+  return Math.max(...array)
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  if (array === undefined || array[0] === undefined) {
+    return 0
+  }
+  let sum = 0
+  for (let el of array) {
+    sum += el
+  }
+  return sum / array.length
 }
